16b307b7ed46e07dec9836b640cd8f1a7751b9c8
AmineAce <aturserviceassassin@gmail.com>
2026-05-20 16:20:53 +0100

    Update with Vercel URL for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourname.dev',
+  site: 'https://portfolio-lemon-seven-57.vercel.app',
   integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
